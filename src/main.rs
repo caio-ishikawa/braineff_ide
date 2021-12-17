5ce65fe44921c4710b6e32ab5b6dc50f37c517e6
caio-ishikawa <caioarbishikawa@gmail.com>
2021-12-17 16:42:22 +0000

    created output window instead of popup

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,28 @@ mod interpreter;
 
 #[derive(Default, NwgUi)]
 pub struct Editr { 
-    #[nwg_control(size: (600, 600), position: (300, 300), title: "EDITR")]
+    #[nwg_control(size: (600, 300), position: (300, 300), title: "output")]
+    output_window: nwg::Window, 
+
+    #[nwg_control(text:"output", parent: output_window)]
+    output_label: nwg::RichLabel, 
+
+    #[nwg_control(size: (600, 600), position: (300, 300), title: "EDITR", flags: "MAIN_WINDOW|VISIBLE")]
     #[nwg_events(OnInit:[Editr::init_text])]
     window: nwg::Window,
 
-    #[nwg_control(size:(600, 580), position: (0, 20))]
+    #[nwg_control(size:(600, 560), position: (0, 40))]
     code_edit: nwg::RichTextBox, 
 
-    #[nwg_control(text:"Run", size: (200, 20), position: (20, 0))]
-    #[nwg_events(OnButtonClick: [Editr::say_hello])]
+
+    #[nwg_control(text:"Run", size: (100, 40), position: (0, 0))]
+    #[nwg_events(OnButtonClick: [Editr::run_code])]
     run_button: nwg::Button, 
+
+    #[nwg_control(text:"Debug", size: (100, 40), position: (100, 0))]
+    #[nwg_events(OnButtonClick: [Editr::debug])]
+    debug_button: nwg::Button, 
+
 }
 
 impl Editr {
@@ -37,13 +49,18 @@ impl Editr {
         });
     }
 
-    fn say_hello(&self){
+    fn run_code(&self){
         println!("{:?}", self.code_edit.para_format());
         let code = interpreter::lexer(&self.code_edit.text());
         let parsed_code = interpreter::parse(code);
-        let mut output = interpreter::compile(parsed_code);
-        let output_sliced = output.as_str();
-        nwg::modal_info_message(&self.window, &output, &format!("OUTPUT: {}", output));
+        let output = interpreter::compile(parsed_code);
+        let output_box = &self.output_label;
+        output_box.set_text(&output);
+        //nwg::simple_message("Output", &output);
+    }
+
+    fn debug(&self){ 
+        println!("DEBUG MODE");
     }
 }
 
